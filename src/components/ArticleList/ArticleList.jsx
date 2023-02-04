c355ab5967310198b44da5e66adeaaa0e803fe88
Alexa Dupin <alexa.dupin@gmail.com>
2023-02-04 14:22:17 +0100

    Feat: refresh every 5min

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -12,23 +12,24 @@ function ArticleList({
 }) {
   const [results, setResults] = useState([]);
 
-  const fetchArticle = async () => {
-    try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&sortBy=publishedAt&apiKey=1ce0e4832cb6431991be94fefd1c5b62`);
-      setResults(response.data.articles);
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Fetch articles when country changes
-  useEffect(
-    () => {
-      fetchArticle();
-    },
-    [country],
-  );
+  // FetchArticle on API depending on country
+  useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&sortBy=publishedAt&apiKey=1ce0e4832cb6431991be94fefd1c5b62`);
+        setResults(response.data.articles);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchArticle();
+    // Refresh every 5 min and clear interval
+    const interval = setInterval(fetchArticle, 300 * 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [country]);
 
   return (
     <div className="articles__list">
